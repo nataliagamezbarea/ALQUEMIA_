858b1618352eab7268cab3925028e19d465932e4
nataliagamezbarea <nataliagbarea@gmail.com>
2025-05-06 01:18:42 +0200

    Agregar rutas y lógica para autenticación de usuarios, incluyendo inicio de sesión, cierre de sesión, recuperación y restablecimiento de contraseña

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
 import os
 from flask import Flask
-from backend.base_de_datos import init_models  
-from routes.home import home
+from backend.base_de_datos import db, init_models
+
+# Importamos las funciones para las vistas
+from routes.authentication  import login , olvidado_contraseña ,  registro , restablecer_contraseña
+from routes import home 
 
 app = Flask(__name__)
+
+# Configuramos los datos para enviar correos (esto se toma desde variables de entorno)
+app.config.update(
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_PORT=os.getenv("MAIL_PORT"),
+    MAIL_USE_TLS=os.getenv("MAIL_USE_TLS"),
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_DEFAULT_SENDER=os.getenv("MAIL_DEFAULT_SENDER")
+)
+
+# Esta clave es para poder usar sesiones seguras dentro de Flask
+app.secret_key = os.urandom(24)
 
 # Datos de conexión a la base de datos MySQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/tienda_online'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Clave aleatoria para proteger las sesiones del usuario
-app.secret_key = os.urandom(24)
-
 # Carga y refleja las tablas de la base de datos
 init_models(app)
 
 # Ruta principal de la app
-app.add_url_rule('/', 'home', home)
+app.add_url_rule("/", "home", home)
+app.add_url_rule("/login", "login", login, methods=["GET", "POST"])
+app.add_url_rule("/registro", "registro", registro, methods=["GET", "POST"])
+app.add_url_rule("/olvidado_contraseña", "olvidado_contraseña", olvidado_contraseña, methods=["GET", "POST"])
+app.add_url_rule("/restablecer_contraseña/<token>", "restablecer_contraseña", restablecer_contraseña, methods=["GET", "POST"])
 
 # Lanza la app en modo desarrollo
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ routes/authentication/__init__.py
@@ -0,0 +1,4 @@
+from .login import login
+from .olvidado_contraseña import olvidado_contraseña
+from .registro import registro
+from .restablecer_contraseña import restablecer_contraseña
